fe2c84926bbdf3022d66cb595048a2f1ba27258b
Rishabh Singh <rishthakur18@gmail.com>
2024-07-15 04:55:45 +0530

    ad

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost', 
     '127.0.0.1', 
-    'todo-list-load-balancer-1292925265.ap-south-1.elb.amazonaws.com'
+    'todo-list-load-balancer-1219772498.us-east-1.elb.amazonaws.com'
 ]
 
 
